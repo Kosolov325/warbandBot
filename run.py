57ee715edb221df46d2541288002ded3a6c9f101
root <root@mirauqui.pzxc5mvbrwfunpo1katlwsi0ta.nx.internal.cloudapp.net>
2021-01-12 21:03:56 +0000

    warbandBot

--- run.py
+++ run.py
@@ -6,13 +6,13 @@ from discord.ext import tasks
 
 client = discord.Client()
 
-token = "NzAyNzA5NjEzMTU3NTQ4MDk4.XqGi3A.J9dBrkd8kkuXnpDosVubtlWmwII"
+token = "###"
 
 @tasks.loop(minutes=1)
 async def status():
-    channel = client.get_channel(int("####"))
+    channel = client.get_channel(int("797974917865013248"))
     try:
-        url = urllib.request.urlopen('###')
+        url = urllib.request.urlopen('http://localhost:7240/')
         xml = ET.parse(url)
         root = xml.getroot()
 
@@ -26,13 +26,29 @@ async def status():
         for mapa in [ '.MapName' ]:
             value3 = xml.find(mapa);
             str3 = ET.tostring(value3, method="text").decode()
+        for passw in [ '.HasProtectedPassword' ]:
+            value4 = xml.find(passw);
+            str4 = ET.tostring(value4, method="text").decode()
+            if (str4 == "yes"):
+                whitelist=True
+            else:
+                whitelist=False
 
-        embed=discord.Embed(title="WarbandBot", color=0x38b10b)
+        if (whitelist == True):
+            cor=0xf46a01
+        else:
+            cor=0x38b10b
+        embed=discord.Embed(title="WarbandBot", color=cor)
         embed.set_author(name="WarbandBot", icon_url="https://media.moddb.com/images/mods/1/37/36148/Asset_23.png")
         embed.set_thumbnail(url="https://media.moddb.com/images/mods/1/37/36148/Asset_23.png")
         embed.add_field(name="Name", value=str1, inline=True)
         embed.add_field(name="Players", value=str2, inline=True)
         embed.add_field(name="Map", value=str3, inline=True)
+        if (whitelist == True):
+            embed.add_field(name="Senha", value="Sim", inline=True)
+            embed.set_footer(icon_url = 'https://i.pinimg.com/originals/72/a8/91/72a8910613b3df0b066ad2fabf1284a2.png', text='Ops... talvez o servidor esteja em manuntenção') 
+        else:
+            embed.add_field(name="Senha", value="Não", inline=True)
         await channel.purge()
         await channel.send(embed=embed);
     except:
@@ -42,6 +58,7 @@ async def status():
         embed.add_field(name="Name", value="offline", inline=True)
         embed.add_field(name="Players", value="offline", inline=True)
         embed.add_field(name="Map", value="offline", inline=True)
+        embed.add_field(name="Senha", value="offline", inline=True)
         await channel.purge()
         await channel.send(embed=embed)
 
